10e76e2c76e53af368f56f6ae9f7c43a3b9dae76
1OmarTarek1 <cr7omartarek@gmail.com>
2024-07-22 03:19:01 +0300

    edit nav and side

--- src/Sections/NavbarSec/NavbarSec.jsx
+++ src/Sections/NavbarSec/NavbarSec.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import Headroom from 'react-headroom';
+import { useTheme } from '../../Components/ThemeProvider/ThemeProvider';
 import { LangBtn, NavBtn, NavLinks, ProfileLink, Search } from '../../Components';
 import { MdSearchOff, MdSearch } from "react-icons/md";
-import { FaBell, FaCartShopping, FaHeart, FaList } from 'react-icons/fa6';
+import { FaBell, FaCartShopping, FaHeart, FaList, FaMoon, FaSun } from 'react-icons/fa6';
 import './NavbarSec.css';
 
 const NavbarSec = ({ isLoading, setIsLoading, toggleSidebar }) => {
@@ -12,50 +13,47 @@ const NavbarSec = ({ isLoading, setIsLoading, toggleSidebar }) => {
   const [search, setSearch] = useState(false);
   const [searchValue, setSearchValue] = useState("");
   const navBtnRef = useRef(null);
+  const { theme, toggleTheme } = useTheme();
   const searchIconRef = useRef(null);
 
   const handleOpenNav = () => {
     setSearchValue("");
     setOpenNav(!openNav);
-    
   };
+
   const handleOpenSearch = () => {
     setSearchValue("");
     setSearch(!search);
 
     // setTimeout to make sure the input is rendered
-    setTimeout(() => { 
+    setTimeout(() => {
       const searchPC = document.querySelector('#pcSearch');
       if (searchPC) {
         searchPC.focus();
       }
     }, 100);
-
   };
-  
-
-
 
   useEffect(() => { // Close Links when click a (navLink) and (outside navbar)
     const handleClickOutside = (event) => {
       const NestedLink = document.querySelector(".NestedLink");
       const navLinks = document.querySelector('.navMob');
       if (
-        openNav && 
+        openNav &&
         navLinks &&
         !navLinks.contains(event.target) &&
         !navBtnRef.current.contains(event.target) &&
         !NestedLink.contains(event.target)
       ) {
-        setOpenNav(false); 
+        setOpenNav(false);
         setSearchValue("");
       }
     };
     document.addEventListener('mousedown', handleClickOutside);
 
     let NavLinks = document.querySelectorAll(".clsLink");
-    let clsNav = () => { 
-      setOpenNav(false); 
+    let clsNav = () => {
+      setOpenNav(false);
       setSearchValue("");
     };
     NavLinks.forEach(NavLink => {
@@ -70,11 +68,10 @@ const NavbarSec = ({ isLoading, setIsLoading, toggleSidebar }) => {
     };
   }, [openNav]);
 
-
   useEffect(() => { // Close Search Box when clicking outside
     const handleClickOutside = (event) => {
-      let searchIcn  = document.querySelector(".icnWrapperSrch ");
-      let searchPc  = document.querySelector(".pcSearch ");
+      let searchIcn = document.querySelector(".icnWrapperSrch ");
+      let searchPc = document.querySelector(".pcSearch ");
       if (search && searchPc && !searchPc.contains(event.target) && !searchIcn.contains(event.target)) {
         setSearch(false);
       }
@@ -85,7 +82,6 @@ const NavbarSec = ({ isLoading, setIsLoading, toggleSidebar }) => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [search]);
-
 
   useEffect(() => { // Control Media From Here
     const handleResize = () => {
@@ -103,118 +99,156 @@ const NavbarSec = ({ isLoading, setIsLoading, toggleSidebar }) => {
     };
   }, [setMobMedia]);
 
-
-  
   return (
     <Headroom>
       <nav className='navbarSec'>
         <div className="navContainer">
 
           {/*SideBtn & NavBtn & LangBtn & LOGO & Search-Icon */}
-          <div 
-          className="logoBX d-flex"
-          style={{alignItems:"center", gap:"5px"}}
+          <div
+            className="logoBX d-flex"
+            style={{ alignItems: "center", gap: "5px" }}
           >
-            <div 
-            className="sideBtn"
-            onClick={toggleSidebar}
+            <div
+              className="sideBtn"
+              onClick={toggleSidebar}
             >
               <FaList />
             </div>
-            
+
             <NavLink to='/' className="logo clsLink">
               <span>LOGO</span>
             </NavLink>
 
-              <LangBtn 
-              isLoading={isLoading} 
+            <LangBtn
+              isLoading={isLoading}
               setIsLoading={setIsLoading}
-              />
-
-              <div
-                  className = { search ? "icnWrapperSrch active" : "icnWrapperSrch" }
-                  onClick={handleOpenSearch}
-                  ref={searchIconRef}
-                >
-                  {search 
-                    ? <MdSearchOff />
-                    : <MdSearch />
-                  }
+            />
+            {mobMedia && (
+              <div className="icnWrapperSrch themWrapper">
+                <label className='theWrapper' style={{cursor:"pointer"}}>
+                  <input
+                    style={{ opacity: "0", width: "0", height: "0" }}
+                    type="checkbox"
+                    id="mainTheme2"
+                    checked={theme === 'light'} // Reflect current theme state
+                    onChange={toggleTheme}
+                  />
+                  <span className="theIcn" >
+                    {theme === 'light'
+                      ? <FaMoon style={{position:"relative", top:"-1px"}} 
+                        data-aos="fade" data-aos-duration="500"
+                      />
+                      : <FaSun  style={{position:"relative", top:"-1px"}} 
+                        data-aos="fade" data-aos-duration="500"
+                      />
+                    }
+                  </span>
+                </label>
               </div>
+            )}
+
+            <div
+              className={search ? "icnWrapperSrch active" : "icnWrapperSrch"}
+              onClick={handleOpenSearch}
+              ref={searchIconRef}
+            >
+              {search
+                ? <MdSearchOff />
+                : <MdSearch />
+              }
+            </div>
           </div>
 
-
           {!mobMedia && ( // Nav Links
-            <ul className="navLinks" >
-              <NavLinks/>
+            <ul className="navLinks">
+              <NavLinks />
             </ul>
           )}
 
-
           {/* Profile-Link & Notifications & Shope-cart & Favorites & Search-Icon */}
-          <div 
-            className="navIcnWrapper" 
-            style={{justifyContent:"center", alignItems:"center"}}
+          <div
+            className="navIcnWrapper"
+            style={{ justifyContent: "center", alignItems: "center" }}
           >
+            <div className="icnWrapperSrch">
+              <label className='theWrapper' style={{cursor:"pointer"}}>
+                <input
+                  style={{ opacity: "0", width: "0", height: "0" }}
+                  type="checkbox"
+                  id="mainTheme"
+                  checked={theme === 'light'} // Reflect current theme state
+                  onChange={toggleTheme}
+                />
+                <span className="theIcn" >
+                  {theme === 'light'
+                    ? <FaMoon style={{position:"relative", top:"-1px"}} 
+                      data-aos="fade" data-aos-duration="500"
+                    />
+                    : <FaSun  style={{position:"relative", top:"-1px"}} 
+                      data-aos="fade" data-aos-duration="500"
+                    />
+                  }
+                </span>
+              </label>
+            </div>
 
-            <NavLink 
-            className="navProfIcon HeartIcn" 
-            to={"/Favorites"}
+            <NavLink
+              className="navProfIcon HeartIcn"
+              to={"/Favorites"}
             >
               <FaHeart />
             </NavLink>
 
-            <NavLink 
-            className="navProfIcon shopCarIcn" 
-            to={"/MyShop"}
+            <NavLink
+              className="navProfIcon shopCarIcn"
+              to={"/MyShop"}
             >
               <FaCartShopping />
             </NavLink>
 
-            <NavLink 
-            className="navProfIcon NotifyIcn" 
-            to={"/Notifications"}
+            <NavLink
+              className="navProfIcon NotifyIcn"
+              to={"/Notifications"}
             >
               <FaBell />
             </NavLink>
 
-
-            <ProfileLink setIsLoading={setIsLoading}/>
+            <ProfileLink setIsLoading={setIsLoading} />
 
             {mobMedia && (
-              <NavBtn 
-                ref={navBtnRef} 
-                openNav={openNav} 
-                setOpenNav={setOpenNav} 
-                handleOpenNav={handleOpenNav} 
+              <NavBtn
+                ref={navBtnRef}
+                openNav={openNav}
+                setOpenNav={setOpenNav}
+                handleOpenNav={handleOpenNav}
               />
-            )} 
+            )}
           </div>
         </div>
 
-
         <div className="searchPcWrapper">
-          <Search 
-            searchValue={searchValue} 
-            setSearchValue={setSearchValue} 
-            cla={search 
-                  ? 'search pcSearch opened' 
-                  : 'search pcSearch'
+          <Search
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            cla={search
+              ? 'search pcSearch opened'
+              : 'search pcSearch'
             }
             id={"pcSearch"}
           />
         </div>
 
         {mobMedia && ( // Mobile NavLinks
-          <ul className={ openNav ? ("navLinks navMob opened") : ("navLinks") }>
+          <ul className={openNav ? ("navLinks navMob opened") : ("navLinks")}>
             <NavLinks />
-            <Search 
-            searchValue={searchValue} 
-            setSearchValue={setSearchValue} 
-            cla={"search mobSearch"} 
-            id={"mbSearch"} 
+            <Search
+              searchValue={searchValue}
+              setSearchValue={setSearchValue}
+              cla={"search mobSearch"}
+              id={"mbSearch"}
             />
-          </ul> 
+          </ul>
         )}
       </nav>
     </Headroom>
